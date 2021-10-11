71c712e897e3798c4d06d5d111b74a0645e5e2ea
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2021-10-11 16:50:28 -0700

    added comments

--- solution.py
+++ solution.py
@@ -9,13 +9,13 @@ import re
 REGEX = re.compile('[%s]' % re.escape(string.punctuation))
 
 
-# accept input as files or line-by-line
+# accept input as files or line-by-line from pipe
 def main():
 
     # process stdin
     if not sys.argv[1::] or len(sys.argv[1::]) == 0:
 
-        # checks if the stdin is not referring to terminal input
+        # process the stdin from pipe if it's not referring to terminal inputs
         if not sys.stdin.isatty():
             print('>>>>>>> Process from stdin <<<<<<<')
             process_stdin(sys.stdin.read())
@@ -29,6 +29,8 @@ def main():
             print(f'>>>>>>> {f} <<<<<<<')
             process_file(f)
 
+# reads from the stdin and print out the list of common sequences
+
 
 def process_stdin(buff):
     dictionary = {}
@@ -38,35 +40,41 @@ def process_stdin(buff):
     print_result(dictionary)
 
 
+# reads from a file and print out the list of common sequences
 def process_file(target):
     dictionary = {}
 
     with open(target, 'r') as f:
         s = ''
 
+        # storing the file content to a string variable in case of a large file
         for line in f:
             s += line
-            # print(s)
 
     matches = find_matches_as_list(s)
     dictionary = get_map_of_all_content(matches)
     print_result(dictionary)
 
 
-# process the text and return the valid words as a list
+# takes in a string of text and returns a list of valid words
 def find_matches_as_list(line):
     matches = []
 
+    # removing punctuactions from a string using regular expression
     res = REGEX.sub('', line)
 
+    # append each word from the string after removing white spaces
     for word in res.split():
         matches.append(word.lower())
 
     return matches
 
 
+# takes in a list of valid words and returns a dictionary/map of phrases with count as value
 def get_map_of_all_content(matches):
     d = {}
+
+    # insert the current word and the next two words as a key and their occurrences to the dictionary/map
     for i in range(0, len(matches)-2):
         phrase = get_phrase(matches[i], matches[i+1], matches[i+2])
 
@@ -78,18 +86,21 @@ def get_map_of_all_content(matches):
     return d
 
 
+# takes in three strings and returns a combined string with spaces in between
 def get_phrase(first, second, third):
     return first + ' ' + second + ' ' + third
 
 
+# takes in a dictionary/map and prints out the first 100 most common three word sequences to the screen
 def print_result(d):
     if len(d) > 0:
         counter = 100
 
+        # sorts the dictionary in descending order based on its value (number of appearances)
         sorted_by_values = sorted(d, key=d.get, reverse=True)
 
-        print('Phrase                         | Count')
-        print('--------------------------------------')
+        print('Phrase                      | Count')
+        print('-------------------------------------')
 
         for key in sorted_by_values:
             if counter == 0:
@@ -99,12 +110,12 @@ def print_result(d):
 
             counter -= 1
 
-        print('----------------------------------------\n')
+        print('-------------------------------------\n')
 
     else:
-        print('----------------------------------------')
+        print('-------------------------------------')
         print('No three-word sequences found')
-        print('----------------------------------------\n')
+        print('-------------------------------------\n')
 
 
 if __name__ == '__main__':
